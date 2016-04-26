0eefebd2a383e3c3bbf78a793f648a47f5dea908
Brian Torres <btorres33@mail.valenciacollege.edu>
2016-04-26 15:13:00 -0400

    Bestfriend similator (unfinished)
    Having some trouble on creating another response choice after the if
    statement.

---
+++ devel/BestFriends similator.py
@@ -0,0 +1,31 @@
+#copy and paste to your python shell to run it
+
+#BESTFRIEND similator is to make choices to interact with the computer(your bestfriend).
+
+
+
+import random
+print('BESTFRIEND SIMILATOR.\n')
+    
+print('COMPUTER: How are you today? [Response choice: "ok", "good", "bad"]')
+
+responses = input()
+
+if responses == "ok":
+    print('COMPUTER: just ok?, is it about the final exam?')
+elif responses == 'bad':
+    print('COMPUTER: Oh no! what happen?')
+elif responses == 'good':
+    print('COMPUTER: That\'s wonderful, what made you happy?')
+ 
+
+
+
+
+        
+
+
+        
+    
+
+
