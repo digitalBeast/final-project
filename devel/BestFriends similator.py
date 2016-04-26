878d9227efa2391feb589142c4a1959ffced0647
Brian Torres <btorres33@mail.valenciacollege.edu>
2016-04-26 16:50:53 -0400

    bestfriend similator (unfinished)
    I tried to lead the conversation but started having issue. not sure what
    method to use for this kind of idea i'm creating

--- devel/BestFriends similator.py
+++ devel/BestFriends similator.py
@@ -12,11 +12,18 @@ print('COMPUTER: How are you today? [Response choice: "ok", "good", "bad"]')
 responses = input()
 
 if responses == "ok":
-    print('COMPUTER: just ok?, is it about the final exam?')
+    print('COMPUTER: just ok?, is it about the final exam? [Response choice: "yes", "no", "maybe"]')
+    if responses == "yes":
+            print('Don\'t you worry, you got the skill and spirit to pass it!')
+    elif responses == "no":
+            print('oh ok, hmmm you should be fine then! tell me you like the music?')
+    elif responses == "maybe":
+            print('hmm I see you don\'t know...don\'t be hiding now')
+    
 elif responses == 'bad':
-    print('COMPUTER: Oh no! what happen?')
+    print('COMPUTER: Oh no! what happen? [Response choice: "sick", "failing class", "angry"]')
 elif responses == 'good':
-    print('COMPUTER: That\'s wonderful, what made you happy?')
+    print('COMPUTER: That\'s wonderful, what made you happy? [Response choice: "passing class", "summer time", "just happy"')
  
 
 
