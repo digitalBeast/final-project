180688305054ebad84e9dd91ca3ed45e08ee4f1e
Brian Torres <btorres33@mail.valenciacollege.edu>
2016-04-26 23:04:40 -0400

    BestFriend similator (completed)
    had a struggle to do this project but somehow I made it very simple but
    very long to do.

--- devel/BestFriends similator.py
+++ devel/BestFriends similator.py
@@ -2,31 +2,78 @@
 
 #BESTFRIEND similator is to make choices to interact with the computer(your bestfriend).
 
-
-
-import random
 print('BESTFRIEND SIMILATOR.\n')
     
 print('COMPUTER: How are you today? [Response choice: "ok", "good", "bad"]')
-
+#####START of the conversation#####
 responses = input()
 
+
+#####IF USER type "ok", "good", "bad"######
 if responses == "ok":
-    print('COMPUTER: just ok?, is it about the final exam? [Response choice: "yes", "no", "maybe"]')
-    if responses == "yes":
-            print('Don\'t you worry, you got the skill and spirit to pass it!')
-    elif responses == "no":
-            print('oh ok, hmmm you should be fine then! tell me you like the music?')
-    elif responses == "maybe":
-            print('hmm I see you don\'t know...don\'t be hiding now')
+    print('COMPUTER: just ok?, is it about the final exam? [Response choice: "yes", "no",]')
+elif responses == "good":
+    print('COMPUTER: That is wonderful, what makes you happy? [Response choice: "summer", "just happy"]')
+elif responses == "bad":
+    print('COMPUTER: why you moody today? this semester almost over. [Response choice: "stress, sick"]')
+else:
+    print('COMPUTER: I don\'t know what you talking about? [Restart game to play]')
+
+
+
+#leading to conversation of the topic#
+#####"OK"######
+responses = input()
+if responses == "yes":
+    print('COMPUTER: don\'t worry it might be tough but best way to study is to take breaks to refresh yourself. Want to know more tips on passing the exam? [Response choice: "yes", "no"]')
+elif responses == "no":
+    print('COMPUTER: oh okay goodluck!\n [ restart to play again.]')
+#####"GOOD"######
+elif responses == "summer":
+    print('COMPUTER: its all about the waves and the tanning my friend, ahh love the beach, but I can\'t swim I might kill someone ._.\n [restart to play again]')
+elif responses == "just happy":
+    print('COMPUTER: Glad that my bestfriend is happy, make the best of it in your life!\n [restart to play again]')
+####"BAD"######
+elif responses == "stress":
+    print('COMPUTER: hey now take breaks, it help your brain to refresh itself! [restart to play again]')
+elif responses == "sick":
+    print('COMPUTER: OH NO, ok I search up on google, that if you drink spinach soup it help you feel better and put vicks on your chest too! HOPE YOU FEEL BETTER bestie. [restart to play again]')
+else:
+    print('please enter the valid answer, I may not able to speak that laugauge yet.')
+#####"ok"#######
+responses = input()
+if responses == "yes":
+    print('COMPUTER: Use index cards to help your hard drive expand better!\n')
+if responses == "no":
+    print('COMPUTER: That\'s okay my friend, study hard and never give up!\n')
+################
+print('So, you know who am I? [Response choice: "a creeper","my bestfriend","my pet dog", "EASTER EGG"]')
+
+#####a creeper########
+responses = input()
+if responses == "a creeper":
+    print('COMPUTER:.............I shall sit in the corner and think about my life... [thank you for playing]')
+elif responses == "my bestfriend":
+    print('COMPUTER: Ayeeee that\'s right! I have been your imaginary friend since childhood, keeping you safe...creepy huh? lol [thanks for playing]')
+elif responses == "my pet dog":
+    print('YOUR DOG:  ARFFF ARFF')
+elif responses == "EASTER EGG":
+    print('COMPUTER: since your my bestfriend here a little cool code I found on the bottom of program code. Don\'t include the #, just copy and paste the code, make sure its in notepad not python then save it as (anything.bat), and save it in desktop too. now click on that file and then press any key and watch what happpens. ENJOY')
     
-elif responses == 'bad':
-    print('COMPUTER: Oh no! what happen? [Response choice: "sick", "failing class", "angry"]')
-elif responses == 'good':
-    print('COMPUTER: That\'s wonderful, what made you happy? [Response choice: "passing class", "summer time", "just happy"')
- 
+##################################
 
+# @echo off  
+# pause
+# color 0a
+# mode 1000
 
+# :a
+# echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+# goto a
+
+######################################
+
+    
 
 
         
