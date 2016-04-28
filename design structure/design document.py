d2e84249eee6f53ae4d101f0f2999aa6d46f91a1
Brian Torres <btorres33@mail.valenciacollege.edu>
2016-04-28 11:37:56 -0400

    design document
    show how my idea was created

---
+++ design structure/design document.py
@@ -0,0 +1,27 @@
+#design document
+
+
+# BESTFriend
+
+
+#To start my idea of this design, I took the idea of a game from youtube called "existor".
+
+#This is a similator game all about talking to your bestfriend. The computer have its own experience and know how to cheer up the user.
+
+#So, I wrote down what the Computer says and the users responses. It is alot of thinking to do, so I could type down a mix of respectful, and funny response.
+
+#To Start off it will be, "COMPUTER: how are you today?" [Response choice: "ok", "good", "bad"].
+
+#Then with those responses the COMPUTER will lead into those three responses, and conversations will happen. Keep in mind this is only a demo verison of the game.
+
+#I haven't yet find different method to make the coversations last for awhile until the user decides to quit the game.
+
+#I provided simple method such as "if" and "elif" statement along with the "input" code. I somehow created the program without using define or any method to make it easier.
+
+#Most of these method are in the book of chapter 1 and 2 to create this project.
+
+#As the user progress through the game, The computer will give you tips and sayings to cheer up the user,the computer will get to give you a easter egg hidden in the program if you done the responses correctly.
+
+#Copy and paste to python and run it
+
+#Enjoy!
